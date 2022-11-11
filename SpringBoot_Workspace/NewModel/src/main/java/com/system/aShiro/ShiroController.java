69e16fe8022c179fb54ef295324cb896df6b6bc9
Shell Script <cclapher@gmail.com>
2022-11-11 17:16:25 +0800

    1111

--- SpringBoot_Workspace/NewModel/src/main/java/com/system/aShiro/ShiroController.java
+++ SpringBoot_Workspace/NewModel/src/main/java/com/system/aShiro/ShiroController.java
@@ -13,10 +13,12 @@ import org.apache.shiro.subject.Subject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+@Controller
 public class ShiroController {
 
 //	@Autowired
